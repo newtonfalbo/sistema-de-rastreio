d2a0eb1acea85273fdf1f86a393dfdcfa130259d
Newton Falbo <119447722+newtonfalbo@users.noreply.github.com>
2025-09-02 12:12:05 -0300

    Atualizar o __init__.py

--- sistema-de-rastreio/src/rastreador/__init__.py
+++ sistema-de-rastreio/src/rastreador/__init__.py
@@ -1,18 +1,26 @@
+import random
+
 class Rastreador:
     def __init__(self):
         self.is_tracking = False
+        self.localizacao_atual = None
 
     def iniciar_rastreamento(self):
         self.is_tracking = True
         # Lógica para iniciar o rastreamento
+        print("Rastreamento inciado")
 
     def parar_rastreamento(self):
         self.is_tracking = False
         # Lógica para parar o rastreamento
+        print("Rastreamento parado")
 
     def obter_localizacao(self):
         if self.is_tracking:
             # Lógica para obter a localização atual
-            pass
+           latitude = round(random.uniform(-90, 90), 6)
+           longitude = round(random.uniform(-180, 180), 6)
+           self.localizacao_atual = (latitude, longitude)
+           return sel.localizacao_atual
         else:
-            raise Exception("Rastreamento não iniciado.")
+          raise Exception("Rastreamento não inciado")
